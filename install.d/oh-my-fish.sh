f47fe68906fda5065710dcaeeb4562db839e05d1
João Lucas <jlucas.nsilva@gmail.com>
2020-12-21 14:08:34 -0300

    up

--- install.d/oh-my-fish.sh
+++ install.d/oh-my-fish.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 curl -fL https://get.oh-my.fish | fish
+
+fish -c 'omf install lambda'
+fish -c 'omf install nvm'
--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 
 sudo apt update
 
+install/dotfiles.sh
 install/packages.sh
 install/suckless.sh
 install/go.sh
 install/vim.sh
+install/nvm.sh
 install/oh-my-fish.sh
 
 sudo apt autoremove
