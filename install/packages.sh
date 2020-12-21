8d5837f12ac66124d41e2f4e6a0aea923ade8e41
João Lucas <jlucas.nsilva@gmail.com>
2020-12-21 10:53:27 -0300

    up

--- install/packages.sh
+++ install/packages.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y xinit lightdm --no-install-recommends --no-install-suggests
-
 sudo apt purge -y vim-tiny
 
+cat install/packages-only.list | xargs sudo apt install -y --no-install-recommends --no-install-suggests
 cat install/packages.list | xargs sudo apt install -y 
