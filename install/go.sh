9b96636630d53331a0fdef6cbc7271e920977d84
João Lucas <jlucas.nsilva@gmail.com>
2020-12-21 10:12:37 -0300

    up

--- install/go.sh
+++ install/go.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo wget -qO- https://golang.org/dl/go1.15.6.linux-amd64.tar.gz | tar -C /usr/local -xzf -
+wget -qO- https://golang.org/dl/go1.15.6.linux-amd64.tar.gz | sudo tar -C /usr/local -xzf -
