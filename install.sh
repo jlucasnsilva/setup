4fe5dbee9f2114375300b01247700647e5979d79
João Lucas <jlucas.nsilva@gmail.com>
2020-12-23 20:57:28 -0300

    Added configuration to use NetworkManager as default

--- install.sh
+++ install.sh
@@ -13,5 +13,13 @@ install/brave.sh
 
 sudo apt autoremove
 
-# Makes network manager go crazy and actually help me
+# Forces NetworkManger to manage connections
 sudo touch /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
+sudo cp /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf.bkp
+sudo sed 's/managed=false/managed=true/g' -i /etc/NetworkManager/NetworkManager.conf
+sudo systemctl restart NetworkManager
+
+# Sets NetworkManager as the default network manager for the wifi
+sudo cp /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml.bkp
+sudo cp etc/01-netcfg.yaml /etc/netplan/01-netcfg.yaml
+sudo netplan apply
